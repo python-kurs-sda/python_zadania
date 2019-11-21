4901873b86ebfcb4a588d55af213c299d2af1cdc
mateusz <mateusz@gmail.com>
2019-11-21 20:35:50 +0100

    add other tests

--- zadania_3/tests/test_my_filter.py
+++ zadania_3/tests/test_my_filter.py
@@ -15,6 +15,18 @@ class TestMyFilter(unittest.TestCase):
         func = lambda x: x > 0
         self.assertEqual(my_filter(func, test_list), [])
 
+    def test_upper_letters(self):
+        test_list = ['Ania', 'ala', 'Kasia', 'ola']
+        func = lambda x: x[0].isupper()
+        self.assertEqual(my_filter(func, test_list), ['Ania', 'Kasia'])
+
+    def test_len_gt_five(self):
+        test_list = ['Ala', 'Elzbieta', 'Kalinka', 'Kasia']
+        func = lambda x: len(x) > 5
+        self.assertEqual(my_filter(func, test_list), ['Elzbieta', 'Kalinka'])
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
